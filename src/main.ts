fe9cce47daedb97edf681d379a8b674140ea1241
a11y4ever! <contact@browser-person.com>
2024-10-15 20:52:54 +0200

    add live announcer

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,19 @@
-import { A11yModule } from '@angular/cdk/a11y';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { A11yModule } from "@angular/cdk/a11y";
 import {
-  TitleStrategy,
-  provideRouter,
-  withComponentInputBinding,
-} from '@angular/router';
-import { CustomTitleStrategy } from './app/a11y-title-strategy';
-import { AppComponent } from './app/app.component';
-import { routes } from './routes';
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
+import { importProvidersFrom } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { AppComponent } from "./app/app.component";
+import { routes } from "./routes";
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule, ReactiveFormsModule, A11yModule),
     provideRouter(routes, withComponentInputBinding()),
-    { provide: TitleStrategy, useClass: CustomTitleStrategy },
     provideHttpClient(withInterceptorsFromDi()),
   ],
 }).catch((err) => console.error(err));
