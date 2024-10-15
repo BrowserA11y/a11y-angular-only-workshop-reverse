ae61ffd0cd8eeaf85f3377756657b8c1b70f28d7
a11y4ever! <contact@browser-person.com>
2024-10-15 20:52:16 +0200

    add focus monitor

--- src/app/books-item/book-item.component.ts
+++ src/app/books-item/book-item.component.ts
@@ -1,37 +1,17 @@
-import { CdkMonitorFocus, FocusOrigin } from '@angular/cdk/a11y';
-import {
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  NgZone,
-  Output,
-} from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { Book } from '../books.service';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { Book } from "../books.service";
 
 @Component({
-  selector: 'app-book-item',
-  templateUrl: './book-item.component.html',
-  styleUrls: ['./book-item.component.scss'],
+  selector: "app-book-item",
+  templateUrl: "./book-item.component.html",
+  styleUrls: ["./book-item.component.scss"],
   standalone: true,
-  imports: [CdkMonitorFocus, RouterLink],
+  imports: [RouterLink],
 })
 export class BookItemComponent {
   @Input() public book?: Book;
   @Output() private bookRemoved = new EventEmitter<void>();
-  elementOrigin = this.formatOrigin(null);
-
-  constructor(private _ngZone: NgZone, private _cdr: ChangeDetectorRef) {}
-
-  formatOrigin(origin: FocusOrigin): string {
-    return origin ? origin + ' focused' : 'blurred';
-  }
-
-  // Workaround for the fact that (cdkFocusChange) emits outside NgZone.
-  markForCheck() {
-    this._ngZone.run(() => this._cdr.markForCheck());
-  }
 
   removeBook() {
     this.bookRemoved.emit();
